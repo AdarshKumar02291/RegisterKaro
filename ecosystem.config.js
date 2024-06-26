02a2d9039e29a04fb1157bd2d7f194694ef7ff12
Adarsh <adarsh.kumar02291@gmail.com>
2024-06-26 16:39:33 +0530

    added eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,23 +1,21 @@
 module.exports = {
-  apps: [
-    {
-      script: "npm start",
-    },
-  ],
+  apps : [{
+    script: 'npm start',
+    
+  }],
 
-  deploy: {
-    production: {
-      key: "key.pem",
-      user: "ubuntu",
-      host: "51.21.128.252",
-      ref: "origin/main",
-      repo: "https://github.com/dev-1ce/RegisterKaro.git",
-      path: "/home/ubuntu",
-      "pre-deploy-local": "",
-      "post-deploy":
-        "source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production",
-      "pre-setup": "",
-      ssh_options: "ForwardAgent=yes",
-    },
-  },
+  deploy : {
+    production : {
+      key:"key.pem",
+      user : 'ubuntu',
+      host : '16.16.183.166',
+      ref  : 'origin/main',
+      repo : 'https://github.com/dev-1ce/RegisterKaro',
+      path : '/home/ubuntu',
+      'pre-deploy-local': '',
+      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+      'ssh_options' : 'ForwardAgent=yes'
+    }
+  }
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,8 @@ export default function Home() {
       <About></About>
       <Video></Video>
       <Clients></Clients>
-      <Steps></Steps>
+      <Steps></Steps>Hi Sir,
+      Hope you’re well . I am Adarsh . I am a Software engineer with 2 years of experience. looking for MERN Stack , Frontend , Backend  roles .I started my career with Wipro where I worked as a Project engineer for 1.6 years . Currently I am working as a MERN Stack Developer at NXYIFY Technology Private Limited. I would be obliged if you could connect me with related roles in your company.
       <Blogs></Blogs>
       <Mobile></Mobile>
       <Numbers></Numbers>
