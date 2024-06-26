422251fbb8d0afd0d29bc60945dea2345480b303
Adarsh <adarsh.kumar02291@gmail.com>
2024-06-26 14:23:42 +0530

    final pages-1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
-import Image from "next/image";
+import { Steps } from "./_components/Steps";
+import { Blogs } from "./_components/Blogs";
 import { Header } from "./_components/Header";
 import { Hero } from "./_components/Hero";
+import { Mobile } from "./_components/Mobile";
 import { Services } from "./_components/Services";
 import { About } from "./_components/About";
 import { Video } from "./_components/Video";
@@ -12,13 +14,16 @@ import Numbers from "./_components/Numbers";
 
 export default function Home() {
   return (
-    <div>
+    <div className="max-w-[1540px]">
       <Header />
       <Hero />
       <Services />
       <About></About>
       <Video></Video>
       <Clients></Clients>
+      <Steps></Steps>
+      <Blogs></Blogs>
+      <Mobile></Mobile>
       <Numbers></Numbers>
       <Newsletter></Newsletter>
       <Works></Works>
